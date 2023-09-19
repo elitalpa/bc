6fc16e556ba0f4ca7ae5019def986d3c20a8eeee
elitalpa <elitalpa@gmail.com>
2023-09-19 17:42:06 +0200

    calculator done - adding other math operations

--- projects/js-calculator/script.js
+++ projects/js-calculator/script.js
@@ -1,6 +1,7 @@
 const firstNumber = parseInt(prompt("Enter your first number"));
 const secondNumber = parseInt(prompt("Enter your second number"));
 
-const SUM = firstNumber + secondNumber;
-
-alert(`The SUM is ${SUM}`);
+alert(`The addition of ${firstNumber} and ${secondNumber} is equal to ${firstNumber + secondNumber}
+The substraction of ${firstNumber} and ${secondNumber} is equal to ${firstNumber - secondNumber}
+The multiplication of ${firstNumber} and ${secondNumber} is equal to ${firstNumber * secondNumber}
+The division of ${firstNumber} and ${secondNumber} is equal to ${firstNumber / secondNumber}`);
