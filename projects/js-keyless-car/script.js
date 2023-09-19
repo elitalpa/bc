8fc1a31fa3ba63b649f3479bc3c0f33771520157
elitalpa <elitalpa@gmail.com>
2023-09-19 22:20:32 +0200

    done with js-keyless-car

--- projects/js-keyless-car/script.js
+++ projects/js-keyless-car/script.js
@@ -1,8 +1,19 @@
-const mystery = ""; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+const mystery = "P7seven"; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
 
 // LEAVE THIS CODE ALONE! (pretty please)
 if (mystery[0] === "P" && mystery.length > 5 && mystery.indexOf("7") !== -1) {
   console.log("YOU GOT IT!!!");
 } else {
   console.log("TRY AGAIN");
+}
+
+const age = parseInt(prompt("What is your age ?"));
+if (age < 18) {
+    alert("Sorry, you are too young to drive this car. Powering off");
+} else if (age == 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+} else if (age > 18) {
+    alert("Powering On. Enjoy the ride!");
+} else {
+    alert("Error. Reload the page and try again !");
 }
