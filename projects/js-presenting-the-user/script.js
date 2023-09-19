74ae0e73972c1631d2967dac6960340962165529
elitalpa <elitalpa@gmail.com>
2023-09-19 15:30:34 +0200

    done with presenting the user

--- projects/js-presenting-the-user/script.js
+++ projects/js-presenting-the-user/script.js
@@ -1,3 +1,5 @@
 const name = prompt("What is your name?");
+const age = prompt("What is your age?");
+const place = prompt("Where do you live?");
 
-alert(`Welcome ${name}!`);
+alert(`Welcome ${name}! You are ${age} years old and you live in ${place}.`);
