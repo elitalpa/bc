0c8262447e8b3f18268267a15204275782d3d460
elitalpa <elitalpa@gmail.com>
2023-09-20 09:55:21 +0200

    small correction i  js-what-fruit

--- projects/js-what-fruit/script.js
+++ projects/js-what-fruit/script.js
@@ -12,13 +12,12 @@ switch (fruit) {
     const grapeColor = prompt("Do you prefer purple or green grapes ?");
     if (grapeColor.toLowerCase() == "purple") {
       alert("I don't like purple grapes ^_^\"");
-      break;
     } else if (grapeColor.toLowerCase() == "green") {
       alert("Green grapes are ok ! (•‿•)");
-      break;
     } else {
       alert("I don't understand :/");
     }
+    break;
   default:
     alert(`You said ${input} and I don't understand :/`)
     break;
