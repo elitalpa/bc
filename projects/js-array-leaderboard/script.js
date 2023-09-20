c7e7b0ff0642feeb3ce3645c33fad930554b189f
elitalpa <elitalpa@gmail.com>
2023-09-20 12:12:41 +0200

    adding some extra ';'

--- projects/js-array-leaderboard/script.js
+++ projects/js-array-leaderboard/script.js
@@ -1,9 +1,9 @@
 const leaderboard = ["Harry", "Lua", "Hermione", "Bellatrix"]; //DON'T TOUCH THIS LINE!
 
 // YOUR CODE BELOW HERE:
-console.log("leaderboard array before changes: \n" + leaderboard)
+console.log("leaderboard array before changes: \n" + leaderboard);
 
 leaderboard[1] = "Luna";
 leaderboard[3] = "Draco";
 
-console.log("leaderboard array after changes: \n" + leaderboard)
+console.log("leaderboard array after changes: \n" + leaderboard);
