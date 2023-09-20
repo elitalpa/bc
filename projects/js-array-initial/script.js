435550c75ac097b2b90456956208764e51a04e10
elitalpa <elitalpa@gmail.com>
2023-09-20 12:06:58 +0200

    js-array-initial is done

--- projects/js-array-initial/script.js
+++ projects/js-array-initial/script.js
@@ -8,3 +8,17 @@ const initialArray = [
     "I am a big bad wolf",
     ["one"],
 ];
+
+console.log("initialArray before changes: \n" + initialArray);
+
+initialArray.shift();
+
+initialArray[0] = initialArray.pop();
+initialArray[1] = initialArray[1][1];
+initialArray[2] = initialArray[3][0];
+
+initialArray.pop();
+initialArray.pop();
+initialArray.pop();
+
+console.log("initialArray after changes: \n" + initialArray);
