bc0454d145cef579abd9a0a767f25f8c0df2e9cf
elitalpa <elitalpa@gmail.com>
2023-09-20 12:12:11 +0200

    js-array-planets is done

--- projects/js-array-planets/script.js
+++ projects/js-array-planets/script.js
@@ -1,3 +1,10 @@
 const planets = ["The Moon", "Venus", "Earth", "Mars", "Jupiter"]; //DO NOT TOUCH THIS LINE!
 
 // YOUR CODE GOES BELOW THIS LINE:
+console.log("planets array before changes: \n" + planets);
+
+planets.shift();
+planets.push("Saturn");
+planets.unshift("Mercury");
+
+console.log("planets array after changes: \n" + planets);
