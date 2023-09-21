33911f68a0716fa70aac60f6093ec3bd2022ed87
elitalpa <elitalpa@gmail.com>
2023-09-21 15:45:41 +0200

    start

---
+++ projects/js-for-loops-array-students/script.js
@@ -0,0 +1,14 @@
+const seatingChart = [
+    ["Colas", "Benoit", "Elodie"],
+    ["Sola", "Irina", "Antonino", "Eli"],
+    ["John", "Maryna", "Naike", "Fabrice"],
+];
+  
+// I can use nested loops to do that :
+  
+for (let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i]; // I created a variable "row" just to be cleaner in the declaration of the next loop. You could put seatingChart[i].length, it would be the same thing
+    for (let j = 0; j < row.length; j++) {
+        console.log(row[j]);
+    }
+}
