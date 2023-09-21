6332f167bdff442abe43f28a7c91044c9a1fc4f1
elitalpa <elitalpa@gmail.com>
2023-09-21 15:33:28 +0200

    start

---
+++ projects/js-for-loops-array-users/script.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+      firstName: "Vito",
+      lastName: "Corleone",
+    },
+    {
+      firstName: "William",
+      lastName: "Wallace",
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+    },
+    {
+      firstName: "Amadeus",
+      lastName: "Mozart",
+    },
+    {
+      firstName: "Barack",
+      lastName: "Obama",
+    },
+];
