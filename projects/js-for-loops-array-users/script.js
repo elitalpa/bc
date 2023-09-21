d9666c7516a2cd96e33b783d636fec413715bab8
elitalpa <elitalpa@gmail.com>
2023-09-21 15:42:50 +0200

    done

--- projects/js-for-loops-array-users/script.js
+++ projects/js-for-loops-array-users/script.js
@@ -20,3 +20,13 @@ const users = [
       lastName: "Obama",
     },
 ];
+
+for (let i = 0; i < users.length; i++) {
+    console.log(`${i + 1}. My full name is ${users[i]["firstName"]} ${users[i]["lastName"]}.`);
+}
+
+// 1. My full name is Vito Corleone.
+// 2. My full name is William Wallace.
+// 3. My full name is Harry Potter.
+// 4. My full name is Amadeus Mozart.
+// 5. My full name is Barack Obama.
