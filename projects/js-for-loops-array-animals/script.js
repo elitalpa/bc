46ac32a83862b428bc3bd88bb41975674a7be23b
elitalpa <elitalpa@gmail.com>
2023-09-21 15:32:22 +0200

    it's done

--- projects/js-for-loops-array-animals/script.js
+++ projects/js-for-loops-array-animals/script.js
@@ -1,1 +1,8 @@
 const animals = ["lion", "pangolim", "zebra", "dog", "cat", "bird"];
+let result = "";
+
+for (let i = animals.length - 1; i >= 0; i--) {
+    result += `"${animals[i]}" `;
+}
+
+console.log(result);
