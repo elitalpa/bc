971369454a7b4d128e8927cef166e83cc8c730ad
elitalpa <elitalpa@gmail.com>
2023-09-21 15:23:21 +0200

    adding to the list and js-for-loops-numbers is done

---
+++ projects/js-for-loops-numbers/script.js
@@ -0,0 +1,43 @@
+// for loops exercises
+
+console.log("All the numbers from 1 to 10 :");
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+console.log("\n");
+// 1 2 3 4 5 6 7 8 9 10
+
+console.log("Even numbers until 20 :");
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+}
+console.log("\n");
+// 2 4 6 8 10 12 14 16 18 20 
+
+console.log("Odd numbers until 20 :");
+for (let i = 1; i <= 20; i += 2) {
+    console.log(i);
+}
+console.log("\n");
+// 1 3 5 7 9 11 13 15 17 19 
+
+console.log("Multiple of 10 from 100 to 1 :");
+for (let i = 100; i >= 1; i -= 10) {
+    console.log(i);
+}
+console.log("\n");
+// 100 90 80 70 60 50 40 30 20 10
+
+console.log("Multiple of 5 from 25 to 0 :");
+for (let i = 25; i >= 0; i -= 5) {
+    console.log(i);
+}
+console.log("\n");
+// 25 20 15 10 5 0
+
+console.log("Multiple of 5 from 25 to 0 :");
+for (let i = 10; i <= 1000; i *= 10) {
+    console.log(i);
+}
+console.log("\n");
+// 10 100 1000
